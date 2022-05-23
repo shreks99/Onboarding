9bac218355b56c3662ef04c173948a41a48d90f2
shreyksinha <shrey.sinha@nice.com>
2022-05-24 02:43:54 +0530

    Added utility for datetime(joda)

---
+++ src/main/java/com/shreks/Onboarding/util/DateTimeUtil.java
@@ -0,0 +1,32 @@
+package com.shreks.Onboarding.util;
+
+import org.joda.time.LocalDateTime;
+import org.springframework.stereotype.Component;
+
+import java.sql.Timestamp;
+import java.time.OffsetDateTime;
+
+import static org.joda.time.DateTimeZone.UTC;
+
+@Component
+public class DateTimeUtil {
+
+    /**
+     * This method returns current utc date time.
+     *
+     * @return UTC Date
+     */
+    public Timestamp getCurrentUTCTimestamp() {
+        return new Timestamp(LocalDateTime.now(UTC).toDateTime(UTC).getMillis());
+    }
+
+    /**
+     * This method returns current UTC DateTime based on provided offset time
+     *
+     * @param time OffsetDateTime
+     * @return UTC Date Timestamp of offset Time
+     */
+    public Timestamp getCurrentUTCTimestampFromOffset(OffsetDateTime time) {
+        return new Timestamp(time.toInstant().toEpochMilli());
+    }
+}
